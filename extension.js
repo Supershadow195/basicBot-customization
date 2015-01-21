bfb37c3985c37ab94d21e46aa9169ee0c19c79d0
Miguel (Sup4h) <superhypershadow195@gmail.com>
2015-01-20 19:43:55 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -105,6 +105,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/Supershadow195/basicBot-customization/master/extension.js', extend);
+    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
 
 }).call(this);
